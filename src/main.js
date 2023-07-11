9ab073b0bb6fd91714ce226088bd36b009864d77
Victor Guzman <victor_27edge@hotmail.com>
2023-07-10 21:02:14 -0600

    Configuracion del entorno 2.0 (La buena :) )

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ const API_URL = 'http://www.themovie.com?api_key=' + API_KEY;
 
 console.log('Hola Mundo');
 console.log(API_URL);
+
+var test = "test";
+
+console.log("Hello World con coma",test,"Concatenacion");
