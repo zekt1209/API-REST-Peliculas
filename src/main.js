bac434ad2cbe22c49e56c3d22a9ef2b12e2c567f
Victor Guzman <victor_27edge@hotmail.com>
2023-07-13 22:36:40 -0600

    Agregamos la maquetacion del HTML y CSS al proyecto, lo entendimos y ahora vamos a consumir la API REST de TheMovieDB y implementar la navegacion

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 const API_URL = 'http://www.themovie.com?api_key=' + API_KEY; 
 
 console.log('Hola Mundo');
-console.log(API_URL);
 
-var test = "test";
 
-console.log("Hello World con coma",test,"Concatenacion");
