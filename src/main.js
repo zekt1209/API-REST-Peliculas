bfd5b2ef4b901928f63ed81092e34611ce5a857f
Victor Guzman <victor_27edge@hotmail.com>
2023-07-14 22:02:01 -0600

    Hicimos la primera consulta a la API de TheMovieDB, consumimos el metodo GET de popular movies, recibimos respuesta, creamos el elemento que necesitamos maquetar en HTML y manipulamos el DOM para insertar dinamicamente las peliculas desde JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,52 @@
-const API_URL = 'http://www.themovie.com?api_key=' + API_KEY; 
+//import fetch from "node-fetch";
+import API_KEY from "./secrets.mjs";
+
+//const API_KEY = '17b0beeb5b0f389cd983177de5c30a80';
+const API_URL = 'https://api.themoviedb.org/3'; 
+const imgUrl = 'http://image.tmdb.org/t/p/w300'
 
 console.log('Hola Mundo');
 
+const popularMovies = async (apiUrl) => {
+    try {
+        const response = await fetch(`${apiUrl}/movie/popular?api_key=${API_KEY}`);
+
+        if (response.status != "200") {
+            console.log("Error en status de response, status: " + response.status);
+        } 
+
+        const data = await response.json()
+    
+        console.log(data.results.length);
+        console.log(data.results);
+
+        const movies = data.results;
+
+        
+        const trendingMoviesArticleContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        // Limpiamos el contenedor de peliculas
+        //trendingMoviesArticleContainer.innerHTML = '';
+
+        movies.forEach(movie => {
+
+            const movie_container = document.createElement('div');
+            movie_container.classList.add('movie-container');
+
+            const movieImg = document.createElement('img');
+            movieImg.classList.add('movie-img');
+            movieImg.setAttribute('alt', movie.original_title);
+            movieImg.setAttribute('src', `${imgUrl}${movie.poster_path}`);
+
+            movie_container.appendChild(movieImg);
+            trendingMoviesArticleContainer.appendChild(movie_container);
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+popularMovies(API_URL);
 
---
+++ src/secrets.mjs
@@ -0,0 +1,45 @@
+// Add it as a parameter
+export const API_KEY = '17b0beeb5b0f389cd983177de5c30a80';
+
+
+export default API_KEY;
+
+/*
+
+{
+    page: 1,
+    results: [
+      {
+        adult: false,
+        backdrop_path: '/5YZbUmjbMa3ClvSW1Wj3D6XGolb.jpg',
+        genre_ids: [Array],
+        id: 447365,
+        original_language: 'en',
+        original_title: 'Guardians of the Galaxy Vol. 3',
+        overview: 'Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.',
+        popularity: 4435.867,
+        poster_path: '/r2J02Z2OpNTctfOSN1Ydgii51I3.jpg',
+        release_date: '2023-05-03',
+        title: 'Guardians of the Galaxy Vol. 3',
+        video: false,
+        vote_average: 8.1,
+        vote_count: 2945
+      },
+      {
+        adult: false,
+        backdrop_path: '/woJbg7ZqidhpvqFGGMRhWQNoxwa.jpg',
+        genre_ids: [Array],
+        id: 667538,
+        original_language: 'en',
+        original_title: 'Transformers: Rise of the Beasts',
+        overview: 'When a new threat capable of destroying the entire planet emerges, Optimus Prime and the Autobots must team up with a powerful faction known as the Maximals. With the fate of humanity hanging in the balance, humans Noah and Elena will do whatever it takes to help the Transformers as they engage in the ultimate battle to save Earth.',
+        popularity: 5409.104,
+        poster_path: '/gPbM0MK8CP8A174rmUwGsADNYKD.jpg',
+        release_date: '2023-06-06',
+        title: 'Transformers: Rise of the Beasts',
+        video: false,
+        vote_average: 7.3,
+        vote_count: 989
+      },
+
+      */
